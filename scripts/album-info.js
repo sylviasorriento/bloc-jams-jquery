8e74393b46731e3270853dfcb41be4ea5d60cc4d
Sylvia <sylviasorriento@gmail.com>
2020-05-08 14:33:37 -0600

    Assignment 15 completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src',album.albumArtUrl);
+  $('.artist').text(album.artist);
+  $('div#release-info').text(album.releaseInfo);
 }
