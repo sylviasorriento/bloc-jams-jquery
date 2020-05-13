8bceac3eae4a7eb17a81982ff1c093b5ccf0d6d4
Sylvia <sylviasorriento@gmail.com>
2020-05-13 10:24:07 -0600

    assignment 17 update

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -20,7 +20,7 @@
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex < album.songs[0]) { return; }
+    if (previousSongIndex < 0) { return; }
 
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
